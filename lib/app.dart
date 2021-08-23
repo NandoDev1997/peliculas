a796e5fd879deea7530507988c514970dbdd925a
NandoDev1997 <luisfernandomoralescortes1997@gmail.com>
2021-08-23 14:26:24 -0500

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/screens/details_screen.dart';
+import 'package:peliculas/screens/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Peliculas',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => HomeScreen(),
+        'details': (_) => DetailsScreen(),
+      },
+    );
+  }
+}
